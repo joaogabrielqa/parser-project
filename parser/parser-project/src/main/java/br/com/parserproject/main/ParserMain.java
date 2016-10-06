fbbb277b1f385f399eba037f397be850fa162fda
joao.gabriel <joao.gabriel@infototal.com.br>
2016-10-06 14:42:27 -0300

    Verificação de valores com json-simple

--- parser/parser-project/src/main/java/br/com/parserproject/main/ParserMain.java
+++ parser/parser-project/src/main/java/br/com/parserproject/main/ParserMain.java
@@ -2,6 +2,7 @@ package br.com.parserproject.main;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -23,16 +24,20 @@ public class ParserMain {
 			PostgreSQLComm pgConn = new PostgreSQLComm();
 			JSONParser parser = new JSONParser();
 			
-			ResultSet rs = pgConn.getResultSet("select dado from aud.auditoria where dado->>'tabela' = 'produto' LIMIT 10");
+			ResultSet rs = pgConn.getResultSet("select * from aud.auditoria where dado->>'tabela' = 'produto' and dado->>'tipo' = 'novo registro'");
 			while(rs.next()){
 //				System.out.println(rs.getString("id_auditoria"));
+				Long idAuditoria = rs.getLong("id_auditoria");
+				Timestamp data = rs.getTimestamp("data_auditoria");
 				Object obj = parser.parse(rs.getString("dado"));
 				JSONObject jsonObj = (JSONObject) obj;
 				JSONObject jsonValores = (JSONObject) jsonObj.get("valores");
 				String valor = jsonObj.get("valores").toString();
-				Double custoReal = (Double) jsonValores.get("custo_real");
+//				Double custoReal = (Double) jsonValores.get("custo_real");
+				Long idProduto = (Long) jsonValores.get("id_produto");
 				//System.out.println(valor);
-				System.out.println("Custo real: " + custoReal);
+//				System.out.println("Custo real: " + idProduto);
+				System.out.println("Id auditoria: "+ idAuditoria + " Data: " + data + " Id produto: " + idProduto);
 			}
 			
 			
